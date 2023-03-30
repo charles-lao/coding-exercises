92626192b027e020c3f58de550774af27fef2782
Charles Lao <charles.h.lao@gmail.com>
2023-03-31 00:32:39 +0800

    Create class-basics.js

---
+++ javascript-exercises/class-basics.js
@@ -0,0 +1,138 @@
+// basic syntax
+
+class MyClass {
+    //class methods
+    constructor() { ... }
+    method1() { ... }
+    method2() { ... }
+    method3() { ... }
+    ...
+}
+
+// example
+
+class User {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayHi() {
+        alert(this.name);
+    }
+
+}
+
+// Usage:
+let user = new User("John");
+user.sayHi();
+
+
+// what is a class?
+
+class User {
+    constructor(name) { this.name = name; }
+    sayHi() { alert(this.name); }
+}
+
+// class is a function
+alert(tyoeof User); // function
+
+// ...or, more precisely, the constructor method
+alert(User === User.prototype.constructor); // true
+
+// The methods are in User.prototype, e.g:
+alert(User.prototype.sayHi); // the code of the sayHi method
+
+// there are exactly two methods in the prototype
+alert(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+
+
+
+// Not just a syntactic sugar
+
+//rewriting calss User in pure functions
+
+// 1. Create constructor function
+function User(name) {
+    this.name = name;
+}
+// a functionprototype has "constructor" property by default,
+// so we don't need to create it
+
+// 2. Add the method to prototype
+User.prototype.sayHi = function() {
+    alert(this.name);
+};
+
+// Usage:
+let user = new User("John");
+user.sayHi();
+
+
+// Class Experession
+
+let User = class {
+    sayHi() {
+        alert("Hello");
+    }
+};
+
+// "Named Class Expression"
+// (no such term in the spec, but that's similar to Named Function Expression)
+let User = class MyClass {
+    sayHi() {
+        alert(MyClass); // MyClass name is visible only inside the class
+    }
+};
+
+new User().sayHi(); // works, shows MyClass definition
+
+alert(MyClass); // error, MyClass isn't visible outside of the class
+
+
+//example 2
+
+function makeClass(phrase) {
+    //declare a class and return it
+    return class {
+        sayHi() {
+            alert(phrase);
+        }
+    };
+}
+
+// Create a new class
+let User = makeClass("Hello");
+
+new User().sayHi(); // Hello
+
+
+
+// Getters/setters
+
+class User {
+    
+    constructor(name) {
+        // invokes the setter
+        this.name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (value.length < 4) {
+            alert("Name is too short.");
+            return;
+        }
+        this._name = value;
+    }
+
+}
+
+let user = new User("John");
+alert(user.name); // John
+
+user = new User(""); // Name is too short
