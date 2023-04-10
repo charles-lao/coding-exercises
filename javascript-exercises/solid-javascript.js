7e063fe5ad35bf5c9715645e36527ca7b1d7640e
Charles Lao <charles.h.lao@gmail.com>
2023-04-11 00:00:17 +0800

    Finish lesson on solid-javascript.js

--- javascript-exercises/solid-javascript.js
+++ javascript-exercises/solid-javascript.js
@@ -43,3 +43,78 @@ var products = [
             .appendTo("#products");
     });
 })();
+
+
+// Double Click events
+
+function Event(name) {
+    this._handlers = [];
+    this.name = name;
+}
+Event.prototype.addHandler = function(handler) {
+    this._handlers.push(handler);
+};
+Event.prototype.removeHandler = function(handler) {
+    for (var i = 0; i < handlers.length, i++) {
+        if(this._handlers[i] == handler) {
+            this._handlers.splice(i, 1);
+            break;
+        }
+    }
+};
+Event.prototype.fire = function(eventArgs) {
+    this._handlers.forEach(function(h) {
+        h(eventArgs);
+    });
+};
+
+var eventAggregator = (function() {
+    var events = [];
+
+    function getEvent(eventName) {
+        return $.grep(events, function(event) {
+            return event.name === eventName;
+        })[0];
+    }
+
+    return {
+        publish: function(eventName, handler) {
+            var event = getEvent(eventname);
+
+            if(!event) {
+                event = new Event(eventName);
+                events.push(event);
+            }
+            event.fire(eventArgs);
+        },
+
+        subscribe: function(eventname, handler) {
+            var event = getEvent(eventName);
+
+            if (!event) {
+                event = new Event(eventName);
+                events.push(event);
+            }
+
+            event.addHandler(handler);
+        }
+    };
+})();
+
+function Cart() {
+    var items = [];
+
+    this.addItem = function(item) {
+        items.push(item);
+        eventAggregator.publish("itemAdded", item);
+    };
+
+    var cartView = function() {
+        eventAggregator.subscribe("itemAdded", function(eventArgs) {
+            var newItem = $('<li></li>')
+            .html(eventArgs.getDescription())
+            .attr('id-card', eventArgs.getId())
+            .appendTo("#cart");
+        });
+    }();
+};
