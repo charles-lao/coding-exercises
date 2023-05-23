3f3d58335c97e7d39ca40ae6fb727fd5a2f39e3f
Charles Lao <charles.h.lao@gmail.com>
2023-05-24 00:03:25 +0800

    Add slide change function to slide markers

--- javascript-exercises/image-slider/script.js
+++ javascript-exercises/image-slider/script.js
@@ -1,9 +1,34 @@
 const rightArrow = document.querySelector('#right-arrow');
 const leftArrow = document.querySelector('#left-arrow');
 const slide = document.querySelector('#slide');
+const slideBtn1 = document.querySelector('#sld-btn-1');
+const slideBtn2 = document.querySelector('#sld-btn-2');
+const slideBtn3 = document.querySelector('#sld-btn-3');
 
 
 let currentSlide = 1;
+
+const changeSlideBtns = (currSlide) => {
+    switch (currSlide) {
+        case 1:
+            slideBtn1.textContent = '◉';
+            slideBtn2.textContent = '◎';
+            slideBtn3.textContent = '◎';
+            break;
+        case 2:
+            slideBtn1.textContent = '◎';
+            slideBtn2.textContent = '◉';
+            slideBtn3.textContent = '◎';
+            break;
+        case 3:
+            slideBtn1.textContent = '◎';
+            slideBtn2.textContent = '◎';
+            slideBtn3.textContent = '◉';
+            break;
+        default:
+            break;
+    }
+}
 
 rightArrow.addEventListener('click', (e) => {
     slide.style.opacity = 0;
@@ -18,10 +43,13 @@ rightArrow.addEventListener('click', (e) => {
     const changeSlide = () => {
         slide.style.background = `url("${currentSlide}.jpg")`;
         slide.style.backgroundSize = '100%';
+        slide.style.backgroundPosition = 'center';
         slide.style.opacity = 1;
     }
 
     setTimeout(changeSlide, 650);
+
+    changeSlideBtns(currentSlide);
     
 });
 
@@ -39,9 +67,60 @@ leftArrow.addEventListener('click', (e) => {
     const changeSlide = () => {
         slide.style.background = `url("${currentSlide}.jpg")`;
         slide.style.backgroundSize = '100%';
+        slide.style.backgroundPosition = 'center';
         slide.style.opacity = 1;
     }
 
     setTimeout(changeSlide, 650);
+
+    changeSlideBtns(currentSlide);
     
+});
+
+slideBtn1.addEventListener('click', (e) => {
+    slide.style.opacity = 0;
+    currentSlide = 1;
+
+    const changeSlide = () => {
+        slide.style.background = `url("${currentSlide}.jpg")`;
+        slide.style.backgroundSize = '100%';
+        slide.style.backgroundPosition = 'center';
+        slide.style.opacity = 1;
+    }
+
+    setTimeout(changeSlide, 650);
+
+    changeSlideBtns(currentSlide);
+});
+
+slideBtn2.addEventListener('click', (e) => {
+    slide.style.opacity = 0;
+    currentSlide = 2;
+
+    const changeSlide = () => {
+        slide.style.background = `url("${currentSlide}.jpg")`;
+        slide.style.backgroundSize = '100%';
+        slide.style.backgroundPosition = 'center';
+        slide.style.opacity = 1;
+    }
+
+    setTimeout(changeSlide, 650);
+
+    changeSlideBtns(currentSlide);
+});
+
+slideBtn3.addEventListener('click', (e) => {
+    slide.style.opacity = 0;
+    currentSlide = 3;
+
+    const changeSlide = () => {
+        slide.style.background = `url("${currentSlide}.jpg")`;
+        slide.style.backgroundSize = '100%';
+        slide.style.backgroundPosition = 'center';
+        slide.style.opacity = 1;
+    }
+
+    setTimeout(changeSlide, 650);
+
+    changeSlideBtns(currentSlide);
 });
