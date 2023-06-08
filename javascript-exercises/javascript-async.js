6359c59b694bc713089f48604efd59e758ff7b88
Charles Lao <charles.h.lao@gmail.com>
2023-06-08 23:23:47 +0800

    Finish javascript-async lesson

--- javascript-exercises/javascript-async.js
+++ javascript-exercises/javascript-async.js
@@ -4,3 +4,31 @@ myDiv.addEventListener("click", function(e) {
     console.log("You clicked me!", e);
 });
 
+const array = [1,2,3,4];
+
+array.forEach(callback);
+
+function callback(arg) {
+    console.log(arg*2);
+}
+
+
+
+// promise
+
+new Promise(function(resolve, reject) {
+    setTimeout(function() { resolve(10); }, 2000);
+})
+.then(function(num) { console.log('first then: ', num); return num * 2; })
+.then(function(num) { console.log('second then: ', num); return num * 2;})
+.then(function(num) { console.log('last then: ', num); });
+
+
+
+// other type of promise
+
+const promise = fetch("data/dogs.json")
+.then(resp => resp.json())
+.then(data => console.log(data))
+
+console.log("Our promise: ", promise);
